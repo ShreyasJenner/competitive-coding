38533b9ec458eecfc852435314752add23a6e1a3
Shreyas Jenner <shreyasjenner4@gmail.com>
2025-07-22 21:35:47 +0530

    199;poor soln

---
+++ leetcode/199.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class TreeNode {
+public:
+  int val;
+  TreeNode *left = nullptr;
+  TreeNode *right = nullptr;
+
+  TreeNode() { this->val = 0; }
+
+  TreeNode(int x) { this->val = x; }
+
+  TreeNode(int x, TreeNode *left, TreeNode *right) {
+    this->val = x;
+    this->left = left;
+    this->right = right;
+  }
+};
+
+class Solution {
+public:
+  int height(TreeNode *root) {
+    if (root == nullptr)
+      return -1;
+
+    int l_ht = height(root->left);
+    int r_ht = height(root->right);
+
+    return l_ht > r_ht ? l_ht + 1 : r_ht + 1;
+  }
+
+  vector<int> rightSideView(TreeNode *root) {
+    int ht = height(root);
+    int lvl;
+
+    vector<int> vec, ans;
+    stack<tuple<TreeNode *, int>> stck;
+    TreeNode *ptr;
+
+    for (lvl = 0; lvl <= ht; lvl++) {
+      vec.push_back(-101);
+    }
+
+    stck.push(make_tuple(root, 0));
+    while (!stck.empty()) {
+      ptr = get<0>(stck.top());
+      lvl = get<1>(stck.top());
+      stck.pop();
+
+      if (ptr != nullptr) {
+        stck.push(make_tuple(ptr->left, lvl + 1));
+        stck.push(make_tuple(ptr->right, lvl + 1));
+
+        if (vec[lvl] == -101) {
+          vec[lvl] = ptr->val;
+        }
+      }
+    }
+
+    for (int val : vec) {
+      if (val != -101) {
+        ans.push_back(val);
+      }
+    }
+
+    return ans;
+  }
+};
+
+int main() {
+  Solution s;
+
+  TreeNode t1, t2, t3, t4, t5;
+
+  t5 = TreeNode(5);
+  t4 = TreeNode(4, &t5, nullptr);
+  t3 = TreeNode(3);
+  t2 = TreeNode(2, &t4, nullptr);
+  t1 = TreeNode(1, &t2, &t3);
+
+  vector<int> ans = s.rightSideView(&t1);
+
+  for (auto val : ans) {
+    cout << val << " ";
+  }
+  cout << '\n';
+
+  return 0;
+}
